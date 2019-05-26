2cb2000c9d44622320c96d94eff8cf8a38639a72
Luc Galarneau <lgalarno@gmail.com>
2019-05-25 21:43:57 -0400

    empty db warning

--- tables/templatetags/format_for_google.py
+++ tables/templatetags/format_for_google.py
@@ -2,6 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.filter
+@register.filter(name='format_for_google')
 def format_for_google(quoted_null_string):
     return quoted_null_string.replace("'", '').replace("None","null")
